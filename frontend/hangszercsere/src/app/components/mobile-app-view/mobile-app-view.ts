4b5fddf1bab6ae33cfb90f52d5fc39f3f0f6cba2
GodziXD <sylvester.privatemail@gmail.com>
2026-02-19 09:53:43 +0100

    added mobile view with button in the navbar

---
+++ frontend/hangszercsere/src/app/components/mobile-app-view/mobile-app-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-app-view',
+  standalone: false,
+  templateUrl: './mobile-app-view.html',
+  styleUrl: './mobile-app-view.css'
+})
+export class MobileAppView {
+
+}
